f43277d4c1be8311e09be9f7320bf438175fd2ab
Adriana <hrl.davidson@gmail.com>
2022-01-18 12:58:03 +0100

    lösung

--- challenge_02.js
+++ challenge_02.js
@@ -16,4 +16,41 @@ Alter 13-17 // Cola
 
 /* Getränke-Challenge */
 
- 
+ // Deklaration
+let Name;
+let Alter;
+let Milch;
+let Saft;
+let Cola;
+let Wein;
+
+//Wertzuweisung
+Name = (prompt("Geben sie den Namen ein: "));
+Alter = Number (prompt("Geben sie der alter ein: "));
+
+// Berechnung
+Milch = (Alter <= 5);
+Saft = (Alter >= 6 & Alter <= 12);
+Cola = (Alter >= 13 & Alter <= 17);
+Wein = (Alter >= 18);
+
+// Ausgabe
+
+
+if (Alter <= 5) 
+{
+    console.log(Name + " trinkt Milch");
+}
+
+else if (Alter >= 6 & Alter <= 12) 
+    {
+        console.log(Name + " trinkt Saft");
+    }
+else if (Alter >= 13 & Alter <= 17)
+    {
+        console.log(Name + " trinkt Cola");
+}
+else if (Alter >= 18) 
+{
+    console.log(Name + " trinkt Wein");
+} 
